24fcf8ea7fd5a96914e1d61aea99a63c889ddf28
Aonkunya Chaitup <aonkunya.dev@gmail.com>
2025-12-13 19:51:12 +0700

    refactor(assignment-1): use app module in server

--- assignment-1/src/index.ts
+++ assignment-1/src/index.ts
@@ -1,29 +1,8 @@
-import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
+import app from "./app";
 import connectDB from "./config/database";
-import transactionRoutes from "./routes/transactionRoutes";
-import { errorHandler, notFoundHandler } from "./middlewares/errorHandler";
 
 dotenv.config();
-
-const app: Application = express();
-
-app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-  res.json({
-    message: "Welcome to Transaction API! 🎉",
-    status: "Server is running",
-    endpoints: {
-      transactions: "/api/transactions",
-    },
-  });
-});
-
-app.use("/api/transactions", transactionRoutes);
-
-app.use(notFoundHandler);
-app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
